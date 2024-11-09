cc5aeb9cc8376d06d0a2b7d80c512473b217746a
SPC-Toad <sangyunkiment@gmail.com>
2024-11-09 15:29:09 -0500

    created credit page

---
+++ src/components/Desktop/Credit/data.js
@@ -0,0 +1,90 @@
+const data = [
+    {
+        subject: "C lang Icon", 
+        subject_url: "https://www.flaticon.com/free-icon/c-_9969473?term=c&page=1&position=56&origin=search&related_id=9969473", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    {
+        subject: "HTML ICON", 
+        subject_url: "https://www.flaticon.com/free-icon/html_186320?term=html&page=1&position=6&origin=search&related_id=186320", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    {
+        subject: "CSS Icon", 
+        subject_url: "https://www.flaticon.com/free-icon/css_186319?term=css&page=1&position=13&origin=search&related_id=186319", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    {
+        subject: "JS Icon", 
+        subject_url: "https://www.flaticon.com/free-icon/js_1073463?term=js&page=1&position=17&origin=search&related_id=1073463", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    {
+        subject: "REACT JS Icon", 
+        subject_url: "https://www.flaticon.com/free-icon/javascript_15263157?term=react+js&page=1&position=10&origin=search&related_id=15263157", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    {
+        subject: "Python Icon", 
+        subject_url: "https://www.flaticon.com/free-icon/python_5968350?term=python+programming&page=1&position=1&origin=search&related_id=5968350", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    {
+        subject: "Credit Icon", 
+        subject_url: "https://www.flaticon.com/free-icon/writing_4693295?term=copywrite&related_id=4693295", 
+        author: "Soremba", 
+        author_url: "https://www.flaticon.com/authors/soremba", 
+        license: "Flaticon Terms of Use", 
+        license_url: "https://www.flaticon.com/terms-of-use" 
+    },
+    // {
+    //     subject: "", 
+    //     subject_url: "", 
+    //     author: "", 
+    //     author_url: "", 
+    //     license: "", 
+    //     license_url: "" 
+    // },
+
+]
+
+export default data
+
+
+// https://react.dev/reference/react/useEffect
+// https://react.dev/reference/react/useState
+
+// https://www.freecodecamp.org/news/how-to-use-settimeout-in-react-using-hooks/
+
+// Copyright
+// https://www.flaticon.com/free-icon/c-_9969473?term=c&page=1&position=56&origin=search&related_id=9969473 C lang
+
+// https://www.flaticon.com/free-icon/html_186320?term=html&page=1&position=6&origin=search&related_id=186320 html
+
+// https://www.flaticon.com/free-icon/css_186319?term=css&page=1&position=13&origin=search&related_id=186319 css
+
+// https://www.flaticon.com/free-icon/js_1073463?term=js&page=1&position=17&origin=search&related_id=1073463 js
+
+// https://www.flaticon.com/free-icon/javascript_15263157?term=react+js&page=1&position=10&origin=search&related_id=15263157 react js
+
+// https://www.flaticon.com/free-icon/python_5968350?term=python+programming&page=1&position=1&origin=search&related_id=5968350 python
+
+// https://www.flaticon.com/free-icon/writing_4693295?term=copywrite&related_id=4693295
---
+++ src/components/Desktop/modules/Card/Card.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './Card.css'
+
+function Card({ subject, subject_url, author, author_url, license, license_url }) {
+  return (
+    <div id='card-container'>
+        <div id='card-title'>
+            {subject}
+        </div>
+        <hr />
+        <ul id='card-content'>
+            <li>The image of a <a href={subject_url}>{subject}</a></li>
+            <li>is provided by <a href={author_url}>{author}</a></li>
+            <li>under <a href={license_url}>{license}</a></li>
+        </ul>
+    </div>
+  )
+}
+
+export default Card
+
+
+
+{/* <div>
+        <div>The image of a</div>
+        <a href={subject_url}>{subject}</a>
+        <div> is provided by </div>
+        <a href={author_url}>{author}</a>
+        <div> under </div>
+        <a href={license_url}>{license}</a>
+    </div> */}
